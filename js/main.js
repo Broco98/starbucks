d16b13cf47d9a0766fef8b79a8bc8837f5455bd9
Broco98 <qkrgydus00@naver.com>
2022-01-18 18:19:22 +0900

    공동 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-// document = HTML이라고 생각하면 됨
-
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-    // searchEl을 클릭하면 InputEl을 포커스
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-    // focused 클래스 추가
-    searchEl.classList.add('focused');
-    // html 속성 지정 placeholder = '통합검색'
-    searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-//blur = focus해제
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    // html 속성 지정 placeholder = '통합검색'
-    searchInputEl.setAttribute('placeholder','');
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -150,5 +126,3 @@ spyEls.forEach(function(spyEl){
         .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 현재연도
